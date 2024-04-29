5ef9e3f3e005b063772e8daee9cc56e74c68ba56
renalfarid <enal.farid@gmail.com>
2024-04-29 23:01:31 +0800

    onError and map response

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,63 @@ import { Elysia } from "elysia";
 import { fetchUsers } from "./users/usersHandler";
 import { getConnection } from "./db/dbHandler";
 import { fetchSchools } from "./schools/schoolsHandler";
-import { Response } from "./lib/helper";
+import { ResponseData } from "./lib/helper";
 
 const app = new Elysia();
+const encoder = new TextEncoder();
+
+app.onError(({ code, error, set }) => {
+  if (code === 'NOT_FOUND') {
+    set.status = 404
+    return error
+  } 
+  if (code === 'INTERNAL_SERVER_ERROR') {
+    set.status = 500
+    //const response = Response(false, set.status, {"message": error.message})
+    return error
+  } 
+})
+
+app.mapResponse(({ response, set }) => {
+  const isJson = typeof response === 'object'
+
+  const text = isJson
+      ? JSON.stringify(response)
+      : response?.toString() ?? ''
+
+  set.headers['Content-Encoding'] = 'gzip'
+
+  return new Response(
+      Bun.gzipSync(encoder.encode(text)),
+      {
+          headers: {
+              'Content-Type': `${
+                  isJson ? 'application/json' : 'text/plain'
+              }; charset=utf-8`
+          }
+      }
+  )
+})
 
 app.get("/", () => {
    const welcome = {"message": "Welcome API"};
-   const response = Response(true, 200, welcome);
+   const response = ResponseData(true, 200, welcome);
    return response;
 })
 
 
 app.get("/users", async () => {
-  try {
     const connection = await getConnection();
     const users = await fetchUsers(connection)
-    const response = Response(true, 200, users);
+    const response = ResponseData(true, 200, users);
     return response;
-  } catch (error) {
-    console.log("Error: ", error);
-    const errorResponse = Response(false, 500, error)
-    return errorResponse;
-  }
 });
 
 app.get("/schools", async () => {
-  try {
     const connection = await getConnection();
     const schools= await fetchSchools(connection);
-    const response = Response(true, 200, schools)
+    const response = ResponseData(true, 200, schools)
     return response;
-  } catch (error) {
-    console.log("Error: ", error);
-    const errorResponse = Response(false, 500, error)
-    return errorResponse;
-  }
 });
 
 app.listen(3000);
